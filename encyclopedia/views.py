6aeb177cfbf9897cb114bdd27b58bb336a957036
Patricia <patricia.alfaro@case.edu>
2021-03-08 15:25:00 -0400

    views.py testing

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,7 +7,7 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
-
+    
 def error(request, name):
     return render(request, "encyclopedia/error.html", {
         "entry": name
