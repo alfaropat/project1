e4532f63be73584b6a822305ea50e4116f6a3bb3
Patricia <patricia.alfaro@case.edu>
2021-03-08 16:05:24 -0400

    Search function in views.py adjusted with respect to forms and posting

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from . import util
+from django import forms
 
 
 def index(request):
@@ -8,16 +9,31 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def search(request,name):
-    if util.get_entry(name) != None:
-        return render(request, "encyclopedia/entry.html", {
-            "entry_name": name,
-            "entry_info": util.get_entry(name)
-        })
-    
-    entries_substring = any(name in entry for entry in util.list_entries())
+def search(request):
 
-    return render(request, "encyclopedia/search.html", {
-        "substring": name,
-        "entries": entries_substring
-    })
+    if request.method == "POST":
+        form = NewSearchForm(request.POST)
+
+        if form.is_valid():
+            search = form.cleaned_data["search"]
+
+            if util.get_entry(search) != None:
+                return render(request, "encyclopedia/entry.html", {
+                    "entry_name": search,
+                    "entry_info": util.get_entry(search)
+                })
+
+            else:
+                search_list = any(search in entry for entry in util.list_entries())
+                
+                return render(request, "encyclopedia/search.html", {
+                    "search_substring": search,
+                    "search_list": search_list
+                })
+                    
+        return render(request, "encyclopedia/search.html", {
+            "form": NewSearchForm()
+        })
+
+class NewSearchForm(forms.Form):
+    search = forms.CharField(label="New Search")
