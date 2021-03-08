881aed252d73fbe0096e990aaad965a9822ed401
Patricia <patricia.alfaro@case.edu>
2021-03-08 15:03:26 -0400

    Changed url path names for random and search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("error", views.error, name="error"),
     path("add", views.add, name="add"),
     path("<str:name>", views.entry, name="entry"),
-    path("entry/<str:name>", views.random, name="random"),
-    path("search/<str:name>", views.search, name="search")
+    path("random", views.random, name="random"),
+    path("search", views.search, name="search")
 ]
