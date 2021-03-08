343968ac1fc77625a534f1098749374b7a22bce3
Patricia <patricia.alfaro@case.edu>
2021-03-08 12:43:49 -0400

    Added edit function (to check with add branch)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,25 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def edit(request,name):
+    if request.method == "POST":
+        
+        form = NewEntryForm(request.POST)
+
+        if form.is_valid():
+            entry_info = form.cleaned_data["entry_info"]
+            util.save_entry(name,entry_info)
+
+            return render(request, "encyclopedia/entry.html", {
+                "name": name
+            })
+
+        else:
+
+            return render(request, "encyclopedia/add.html", {
+                "form": form
+            })
+
+    return render(request, "encyclopedia/html", {
+        "form": NewEntryForm()
+    })
