ee18d458afa9b3b15e43a13a8bb1c9ac9a263260
Patricia <patricia.alfaro@case.edu>
2021-03-08 14:32:44 -0400

    Random function created in views.py and path to random entry was added to urls.py

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,10 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def random(request):
+    entry_chosen = random.choice(util.list_entries())
+
+    return render(request, "encyclopedia/entry.html", {
+        "entry_name": entry_chosen,
+        "entry_info": util.get_entry(entry_chosen)
+    })
