82202b719406b8739f7252403b9ad716da7c5cbf
Patricia <patricia.alfaro@case.edu>
2021-03-08 12:08:31 -0400

    Add function in views.py edited to save forms. New class for new forms created.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from . import util
+from django import forms
 
 
 def index(request):
@@ -8,3 +9,25 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def add(request):
+    if request.method == "POST":
+
+        form = NewEntryForm(request.POST)
+
+        if form.is_valid():
+            entry_name = form.cleaned_data["entry_name"]
+            entry_info = form.cleaned_data["entry_info"]
+
+            util.save_entry(entry_name,entry_info)
+
+            return render(request, "encyclopedia/entry.html", {
+                "entry_name": entry_name,
+                "entry_info": entry_info
+            })
+
+    return render(request, "encyclopedia/add.html", {
+        "form": NewEntryForm()
+    })
+
+class NewEntryForm():
+    entry = forms.CharField(label="New Entry")
