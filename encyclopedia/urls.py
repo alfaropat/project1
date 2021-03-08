95f539fef0d927726406e73a1f6796aa9f1001e7
Patricia <patricia.alfaro@case.edu>
2021-03-08 12:51:34 -0400

    Added path to search page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("search/<str:name>", views.search, name="search")
 ]
