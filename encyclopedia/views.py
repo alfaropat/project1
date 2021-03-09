8c89b65e4735be96424f144162bc01f552e84f9c
Patricia <patricia.alfaro@case.edu>
2021-03-09 11:47:33 -0400

    Added name variable into views.py and index.html (testing)

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -38,12 +38,13 @@ def search(request):
 class NewSearchForm(forms.Form):
     search = forms.CharField(label="New Search")
 
-def entry(request):
-    entry_data = request.method
-    if util.get_entry(entry_data) != None:
+def entry(request, name):
+    #entry_data = request.method
+
+    if util.get_entry(name) != None:
         return render(request, "encyclopedia/entry.html", {
-        "entry_name": entry_data,
-        "entry_info": util.get_entry(entry_data)
+        "entry_name": name,
+        "entry_info": util.get_entry(name)
     })
    # error(request, name)
 
