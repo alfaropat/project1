852dc681efa18074d13419283b465a61aea0d131
Patricia <patricia.alfaro@case.edu>
2021-03-16 14:33:33 -0400

    Fixed search function in views.py for entries that already exist.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,7 +5,7 @@ from django import forms
 from django.http import HttpResponse
 
 def redirect_index(request):
-    return redirect("index")
+    return redirect('index/')
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -18,12 +18,24 @@ def search(request):
         form = NewSearchForm(request.POST)
 
         if form.is_valid():
-            search = form.cleaned_data["search"]
+            form = form.cleaned_data["search"]
 
-            if util.get_entry(search) != None:
+            if util.get_entry(form) != None:
+                full_entry = util.get_entry(form)
+
+                separate_entries = [entry for entry in full_entry.split("#") if entry]
+
+                entry_names = []
+                entry_infos = []
+
+                for entry in separate_entries:
+                    entry_data = [entry_value for entry_value in entry.splitlines() if entry_value]
+                    entry_names.append(entry_data[0].split())
+                    entry_infos.append(entry_data[1])
+
                 return render(request, "encyclopedia/entry.html", {
-                    "entry_name": search,
-                    "entry_info": util.get_entry(search)
+                    "entry_name": entry_names,
+                    "entry_info": entry_infos
                 })
 
             else:
@@ -33,28 +45,26 @@ def search(request):
                     "search_substring": search,
                     "search_list": search_list
                 })
+        else:
+            return render(request, "tasks/search.html", {
+                "form": form
+            })
                     
-        return render(request, "encyclopedia/search.html", {
-            "form": NewSearchForm()
-        })
+    return render(request, "encyclopedia/search.html", {
+        "form": NewSearchForm()
+    })
 
 class NewSearchForm(forms.Form):
     search = forms.CharField(label="New Search")
 
 def entry(request, name):
-    full_entry = util.get_entry(name)
+    full_entry = util.get_entry(name).strip('\n')
 
     if full_entry != None:
         return render(request, "encyclopedia/entry.html", {
             "entry_name": name,
-            "entry_info": full_entry[len(name)+2:len(full_entry)-1]
+            "entry_info": full_entry[len(name)+2:]
         })
-
-   # if util.get_entry(request.GET) != None:
-    #    return render(request, "encyclopedia/entry.html", {
-     #   "entry_name": request.GET,
-      #  "entry_info": util.get_entry(request.GET)
-    #})
    
     return HttpResponse("The value is None!")
 
