562d581071916e292a19bb9969a3627d1d74476f
Patricia <patricia.alfaro@case.edu>
2021-03-29 14:41:41 -0400

    Added comments and adjusted edit and add forms to better display text entry elements.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,18 +2,31 @@ from django.shortcuts import redirect, render
 
 from . import util
 from django import forms
-from django.http import HttpResponse
 import random
 import markdown2
 
+"""
+Redirects default index page to initial URL for project.
+"""
 def redirect_index(request):
     return redirect('/wiki')
 
+"""
+Returns the application's index page with its full list of entries.
+"""
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+"""
+Searches for a particual entry through a POST request.
+If the entry exists in the list of entries, then the user is 
+redirected to the entry page found.
+If the entry is not found in the list of entries, then the user is 
+taken to a resutlts page that displays all entries that contain 
+the searched query as a substring.
+"""
 def search(request):
 
     if request.method == "POST":
@@ -24,10 +37,7 @@ def search(request):
 
             if util.get_entry(form) != None:
                 
-                return render(request, "encyclopedia/entry.html", {
-                    "entry_name": form,
-                    "entry_content": markdown2.markdown(util.get_entry(form))                    
-                })
+                return redirect("encyclopedia:entry",form)
 
             else:
                 search_list = [entry for entry in util.list_entries() if form.lower() in entry.lower()]
@@ -47,12 +57,24 @@ def search(request):
         "form": NewSearchForm()
     })
 
+"""
+Creates a new search form to be used in the search method.
+"""
 class NewSearchForm(forms.Form):
     search = forms.CharField(label="New Search")
 
+"""
+Redirects the user to a random entry page.
+"""
 def redirect_random(request):
     return redirect('encyclopedia:entry', name = random.choice(util.list_entries()))
 
+"""
+Checks if the entry exists in the current list of entries.
+If the entry does exist, then the user is taken to the entry's 
+page in the encyclopedia.
+If the entry does not exist, then the user is taken to an error page.
+"""
 def entry(request, name):
     full_entry = util.get_entry(name)
 
@@ -66,7 +88,16 @@ def entry(request, name):
     return render(request, "encyclopedia/error.html", {
         "entry_name": name
     })
-      
+
+"""
+Allows the user to create a full entry.
+If the entry does not exist in the encyclopedia, then it 
+will be added accordingly.
+If the entry does exist, then the user will be presented 
+an error on the same page.
+Once the entry is successfully saved, the user is redirected 
+to the new entry's page.
+"""      
 def add(request):
     if request.method == "POST":
         form = NewEntryForm(request.POST)
@@ -96,13 +127,17 @@ def add(request):
         "exists": False
     })
 
+"""
+Creates a new entry form to be used in the add method.
+"""
 class NewEntryForm(forms.Form):
     entry_name = forms.CharField(label="Title")
-    entry_info = forms.CharField(label="Content", widget=forms.Textarea(attrs={'style':'width: 80%; height: 75%;'}))
+    entry_info = forms.CharField(label="Markdown Content", widget=forms.Textarea(attrs={'style':'width: 80%; height: 75%;'}))
 
-class NewEditForm(forms.Form):
-    full_entry = forms.CharField(widget=forms.Textarea(attrs={'style':'width: 80%; height: 75%;'}))
-
+"""
+Allows the user to edit an existing entry's current Markdown content.
+Once the entry is saved, the user is redirected to the edited entry.
+"""
 def edit(request,name):
     if request.method == "POST":
         form = NewEditForm(request.POST)
@@ -125,3 +160,9 @@ def edit(request,name):
             "name": name,
             "form": form,
         })
+
+"""
+Creates a new edit form to be used in the edit method.
+"""
+class NewEditForm(forms.Form):
+    full_entry = forms.CharField(label="Markdown Content", widget=forms.Textarea(attrs={'style':'width: 80%; height: 75%;'}))
